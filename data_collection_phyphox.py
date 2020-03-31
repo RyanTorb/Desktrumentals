6f4f12d1fe3761ac0b7055344e2c78a489ca914c
Ryan Torbic <ryan.torbic@lumiscorp.com>
2020-03-31 19:50:43 -0400

    File now reads magnometer measurements

--- data_collection_phyphox.py
+++ data_collection_phyphox.py
@@ -7,7 +7,7 @@ from playsound import playsound
 
 # phyphox configuration
 PP_ADDRESS = "http://192.168.1.8" #change this based on whatever your phone says
-PP_CHANNELS = ["accX", "accY", "accZ"]
+PP_CHANNELS = ["accX", "accY", "accZ", "magX", "magY", "magZ"]
 sampling_rate = 100
 
 # animation and data collection config
@@ -26,6 +26,10 @@ accX = []
 accY = []
 accZ = []
 
+magX = []
+magY = []
+magZ = []
+
 # make one of them true at a time
 isAnimate = False
 isCollectData = True
@@ -37,21 +41,28 @@ def getSensorData():
     accX = data["buffer"][PP_CHANNELS[0]]["buffer"][0]
     accY = data["buffer"][PP_CHANNELS[1]]["buffer"][0]
     accZ = data["buffer"][PP_CHANNELS[2]]["buffer"][0]
+    magX = data["buffer"][PP_CHANNELS[3]]["buffer"][0]
+    magY = data["buffer"][PP_CHANNELS[4]]["buffer"][0]
+    magZ = data["buffer"][PP_CHANNELS[5]]["buffer"][0]
     # print (accX, ' ', accY, ' ', accY)
-    return [accX, accY, accZ]
+    return [accX, accY, accZ, magX, magY, magZ]
 
 # In[11]:
 
 
 # This function is called periodically from FuncAnimation
 def animate(i, xs, accX, accY, accZ):
-    [naccX, naccY, naccZ] = getSensorData()
+    [naccX, naccY, naccZ, nmagX, nmagY, nmagZ] = getSensorData()
 
     xs.append(dt.datetime.now().strftime('%S.%f'))  # %H:%M:%S.%f
 
     accX.append(naccX)
     accY.append(naccY)
     accZ.append(naccZ)
+
+    magX.append(nmagX)
+    magY.append(nmagY)
+    magZ.append(nmagZ)
 
     # plot only the 50 (PREV_SAMPLE) previous smaples
     _accX = accX[-PREV_SAMPLE:]
@@ -72,13 +83,16 @@ def animate(i, xs, accX, accY, accZ):
 
 
 def getData():
-    [naccX, naccY, naccZ] = getSensorData()  # get nth sample
+    [naccX, naccY, naccZ, nmagX, nmagY, nmagZ] = getSensorData()  # get nth sample
     t = dt.datetime.now().strftime('%M:%S.%f')  # %H:%M:%S.%f
     xs.append(t)
     accX.append(naccX)
     accY.append(naccY)
     accZ.append(naccZ)
-    return [t, naccX, naccY, naccZ]
+    magX.append(nmagX)
+    magY.append(nmagY)
+    magZ.append(nmagZ)
+    return [t, naccX, naccY, naccZ, nmagX, nmagY, nmagZ]
 
 
 def main():
@@ -88,17 +102,20 @@ def main():
         plt.show()
     if isCollectData == True:
         while True:
-            [t, naccX, naccY, naccZ] = getData()
+            [t, naccX, naccY, naccZ, nmagX, nmagY, nmagZ] = getData()
             if naccX > 10:
                 print "Note C"
+                print (t, ' ', naccX, ' ', naccY, ' ', naccZ, ' ', nmagX, ' ', nmagY, ' ', nmagZ)
                 playsound('piano-c_C_major.wav')
             elif naccY > 10:
                 print "Note B"
+                print (t, ' ', naccX, ' ', naccY, ' ', naccZ, ' ', nmagX, ' ', nmagY, ' ', nmagZ)
                 playsound('piano-b_B_major.wav')
             elif naccZ > 35:
                 print "Note G"
+                print (t, ' ', naccX, ' ', naccY, ' ', naccZ, ' ', nmagX, ' ', nmagY, ' ', nmagZ)
                 playsound('piano-g_G_major.wav')
-            #print(t, ' ', naccX, ' ', naccY, ' ', naccZ)
+            print (t, ' ', naccX, ' ', naccY, ' ', naccZ, ' ', nmagX, ' ', nmagY, ' ', nmagZ)
             # time.sleep(INTERVALS/1000)   # Delays for INTERVALS seconds.
 
 
